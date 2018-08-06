1dab68f0c6d7fa0242ac4c86260a16313f0c1b6c
Nicolai <organ.nicolai@gmail.com>
2018-08-06 20:01:07 +0300

    built library course component with grid and library grid

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
+import Library from './library/library';
+import Schedule from './schedule/schedule';
+
 class Home extends Component {
   render() {
     return (
       <div className="home">
-        <h1>Course Scheduler</h1>
+        <Library/>
+        <Schedule/>
       </div>
     );
   }
 }
 
-export default Home
+export default Home;
